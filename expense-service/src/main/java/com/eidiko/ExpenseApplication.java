d9e27e69b849bf159ed640bbd737dc9ad2203a73
Jannesh Rao <janneshr007@gmail.com>
2025-09-01 18:50:36 +0530

    One file is unreserved, I don't know why.

---
+++ expense-service/src/main/java/com/eidiko/ExpenseApplication.java
@@ -0,0 +1,13 @@
+package com.eidiko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class ExpenseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseApplication.class, args);
+    }
+}
